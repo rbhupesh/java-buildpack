746092a157ccab75ceab234e94a5296a2e901163
rbhupesh <rbhupesh78@gmail.com>
2018-04-23 21:27:31 +0530

    updating two files

--- lib/java_buildpack/framework/new_relic_agent.rb
+++ lib/java_buildpack/framework/new_relic_agent.rb
@@ -38,7 +38,6 @@ module JavaBuildpack
         credentials   = @application.services.find_service(FILTER, [LICENSE_KEY, LICENSE_KEY_USER])['credentials']
         java_opts     = @droplet.java_opts
         configuration = {}
-		
 	    apply_configuration(credentials, configuration)
         apply_user_configuration(credentials, configuration)
         write_java_opts(java_opts, configuration)
