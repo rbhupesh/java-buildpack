60987b73bf0cedca871942af8e8b0ed5d1963d36
rbhupesh <rbhupesh78@gmail.com>
2018-04-23 15:09:36 +0530

    #

--- lib/java_buildpack/framework/new_relic_agent.rb
+++ lib/java_buildpack/framework/new_relic_agent.rb
@@ -38,7 +38,7 @@ module JavaBuildpack
         credentials   = @application.services.find_service(FILTER, [LICENSE_KEY, LICENSE_KEY_USER])['credentials']
         java_opts     = @droplet.java_opts
         configuration = {}
-		    puts ("<<<<<<<<NewRelicAgent release after empty configuration>>>>>>>>")
+		    #"<<<<<<<<NewRelicAgent release after empty configuration>>>>>>>>"
         apply_configuration(credentials, configuration)
         apply_user_configuration(credentials, configuration)
         write_java_opts(java_opts, configuration)
